b373486b554815f7d07219a4ad00fbb5918217e1
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2020-03-27 22:14:31 +0100

    disable zenity dialog, write credentials to userspace

--- SeafileContentManager/seafilemixin.py
+++ SeafileContentManager/seafilemixin.py
@@ -19,13 +19,14 @@ except:
     raise print('Can not create settings dir. ')
 
 
-
 def getConnection():
-    # Routines to establish the connection to the Seafile Instance,
-    # check credentials, and retriev the library ID.
+    """Read credentials for Seafile endpoint.
 
+    Routines to establish the connection to the Seafile Instance,
+    check credentials, and retriev the library ID.
+    """
     try:
-        with open(BASE + 'settings','r') as file:
+        with open(BASE + 'settings', 'r') as file:
             data = file.read()
         seafileURL = data.split(',')[0].strip()
     except:
@@ -50,34 +51,32 @@ def getConnection():
     print(libraryName)
 
     # Seafile URL
-    seafileURL = os.environ.get('SEAFILE_URL', '')
+
     if seafileURL != '':
-        pass
+        seafileURL = os.environ.get('SEAFILE_URL', '')
     else:
-        seafileURL = 'https://keeper.mpdl.mpg.de'
-        #seafileURLBase = subprocess.check_output(["zenity","--entry","--text",'Please provide the basic Seafile URL (e.g. https://repo.seafile.com):',"--title",'Seafile URL?','--display=:0'])
-        #seafileURL = seafileURLBase.decode("utf-8").strip()
+        # seafileURL = 'https://keeper.mpdl.mpg.de'
+        # seafileURLBase = subprocess.check_output(["zenity","--entry","--text",'Please provide the basic Seafile URL (e.g. https://repo.seafile.com):',"--title",'Seafile URL?','--display=:0'])
+        # seafileURL = seafileURLBase.decode("utf-8").strip()
         #print(seafileURL)
         # os.environ['SEAFILE_URL'] = seafileURL
-        # raise ValueError("Please set the SEAFILE_URL environment variable")
+        raise ValueError("Please set the SEAFILE_URL environment variable")
     # Access Token
-    token = os.environ.get('SEAFILE_ACCESS_TOKEN','')
+
     if token != '':
-        pass
+        token = os.environ.get('SEAFILE_ACCESS_TOKEN', '')
     else:
-        seafileAccount = subprocess.check_output(["zenity","--password","--username","--text",'Please provide your account informations once to obtain an access token:',"--title",'Seafile Account?','--display=:0']).decode("utf-8")
-        account = seafileAccount.split('|')
-        res = requests.post(seafileURL + "/api2/auth-token/", data={'username': account[0], 'password': account[1]})
-        assert res.status_code == 200, 'Could not obtain access token using your credentials.'
-        token = res.json()['token']
-        with open(BASE + 'settings','a') as file:
-            file.write('{0},{1}'.format(seafileURL, token))
-        # os.environ['SEAFILE_ACCESS_TOKEN'] = token
-        #print(token)
-        #raise ValueError("Please set the SEAFILE_ACCESS_TOKEN environment variable")
-    authHeader = {"Authorization":"Token {0}".format(token)}
+        # seafileAccount = subprocess.check_output(["zenity","--password","--username","--text",'Please provide your account informations once to obtain an access token:',"--title",'Seafile Account?','--display=:0']).decode("utf-8")
+        # account = seafileAccount.split('|')
+        # res = requests.post(seafileURL + "/api2/auth-token/", data={'username': account[0], 'password': account[1]})
+        # assert res.status_code == 200, 'Could not obtain access token using your credentials.'
+        # token = res.json()['token']
+        raise ValueError("Please set the SEAFILE_ACCESS_TOKEN environment variable")
+    with open(BASE + 'settings', 'a') as file:
+        file.write('{0},{1}'.format(seafileURL, token))
+    authHeader = {"Authorization": "Token {0}".format(token)}
     # check Authorization
-    res = requests.get(seafileURL + '/api2/auth/ping/', headers = authHeader)
+    res = requests.get(seafileURL + '/api2/auth/ping/', headers=authHeader)
     assert res.text == '"pong"', 'Wrong token {0}, cannot access API at {1}.'.format(token, seafileURL + '/api2')
     # Destination library
     libraryName = os.environ.get('SEAFILE_LIBRARY', 'notebooks')
@@ -85,10 +84,7 @@ def getConnection():
         pass
     else:
         libraryName = 'notebooks'
-        #raise ValueError("Please set the SEAFILE_LIBRARY environment variable")
-    #libraryName = libraryName
-    #
-    resLib = requests.get(seafileURL + '/api2/repos/', headers = authHeader)
+    resLib = requests.get(seafileURL + '/api2/repos/', headers=authHeader)
     idList = [x['id'] for x in resLib.json() if x['name'] == libraryName]
     try:
         # check if idList has one element = library ID exists
@@ -99,7 +95,7 @@ def getConnection():
         data = {'name': libraryName, 'desc': 'new library'}
         createLib = requests.post(
             seafileURL + '/api2/repos/',
-            headers = authHeader, data = data
+            headers=authHeader, data=data
             )
         assert createLib.status_code == 200, 'Cannot create new library, error code {0}'.format(createLib.status_code)
         libraryID = createLib.json()['repo_id']
