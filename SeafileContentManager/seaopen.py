2aeded03ba62bb140d6749fa8dad8994b13fdb5a
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2020-05-06 19:24:42 +0200

    add rudimentary file io

--- SeafileContentManager/seaopen.py
+++ SeafileContentManager/seaopen.py
@@ -1,5 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
+import errno
+import os
 from datetime import datetime
 
 from .seamanager import SeafileContentManager
@@ -12,7 +14,7 @@ class SeafileFS(SeafileContentManager):
     Maps queries like listdir to calls to the Seafile API.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         retVals = getConnection()
 
         self.seafileURL = retVals[0]
@@ -61,6 +63,9 @@ class SeafileFS(SeafileContentManager):
         fileNames = [x['name'] for x in files]
         return fileNames
 
+    def mkdir(self, path=None):
+        pass
+
     def isfile(self, path=None):
         """Return file True or False."""
         try:
@@ -77,3 +82,61 @@ class SeafileFS(SeafileContentManager):
 
     def open(self, path, mode='r'):
         """Open file as byte or str object."""
+
+        if mode in ('w','w+'):
+            return SeafileFileModel(path, mode)
+        elif self.file_exists(path):
+            return SeafileFileModel(path, mode)
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), path
+        )
+
+
+class SeafileFileModel(SeafileContentManager):
+    """Return file like model for Seafile API."""
+
+    def __init__(self, path, mode):
+        retVals = getConnection()
+
+        self.seafileURL = retVals[0]
+        self.authHeader = retVals[1]
+        self.libraryID = retVals[2]
+        self.libraryName = retVals[3]
+        self.serverInfo = retVals[4]
+
+        self.filePath = path
+        self.fileMode = mode
+        if self.fileMode in ('w', 'w+'):
+            # TODO: Create empty file model
+            pass
+        else:
+            self.fileModel = self.getFileModel(path)
+
+    def read(self):
+        """Read file from Seafile API."""
+        if self.fileMode in ('a','r', 't', 'rt'):
+            return self.fileModel['content']
+        if self.fileMode in ('b', 'b+', 'r+b'):
+            return self.fileModel['content'].encode()
+
+    def readlines(self):
+        return self.fileModel['content'].splitlines(True)
+
+    def write(self, content):
+        if self.fileMode == 'a':
+            oldcontent = self.fileModel['content']
+            contentDict = {
+                'content': oldcontent + content
+            }
+            self.fileModel.update(contentDict)
+
+            self.save(self.fileModel, self.filePath)
+        elif self.fileMode in ('w', 'w+'):
+            contentDict = {
+                'content': content
+            }
+            self.fileModel.update(contentDict)
+
+            self.save(self.fileModel, self.filePath)
+        else:
+            raise OSError('Not implemented.')
